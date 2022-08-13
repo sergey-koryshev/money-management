edd243ae19545d4c76d8e66c123047009b8d6249
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-08-13 16:36:43 +0200

    added currencies$ and mainCurrency$ fields of type BehaviorSubject in CurrencyService to be able to subscribe on chenging value in them; implemented method setMainCurrency

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -2,24 +2,38 @@ import { map } from 'rxjs/operators';
 import { Currency } from './../models/currency.model';
 import { CurrencyHttpClientService } from './../http-clients/currency-http-client.service';
 import { Injectable } from '@angular/core';
-import { forkJoin } from 'rxjs';
+import { BehaviorSubject, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
-  currencies: Currency[];
-  mainCurrency: Currency;
+  currencies$: BehaviorSubject<Currency[]>;
+  mainCurrency$: BehaviorSubject<Currency | null>;
+  get currencies() {
+    return this.currencies$.value;
+  }
+  get mainCurrency() {
+    return this.mainCurrency$.value;
+  }
 
-  constructor(private currencyHttpClient: CurrencyHttpClientService) { }
+  constructor(private currencyHttpClient: CurrencyHttpClientService) { 
+    this.currencies$ = new BehaviorSubject<Currency[]>([]);
+    this.mainCurrency$ = new BehaviorSubject<Currency | null>(null);
+  }
 
   load() {
     const currencies$ = this.currencyHttpClient.getAllCurrencies();
     const mainCurrency$ = this.currencyHttpClient.getMainCurrency();
 
     return forkJoin([currencies$, mainCurrency$]).pipe(map(([currencies, mainCurrency]) => {
-      this.currencies = currencies;
-      this.mainCurrency = mainCurrency;
+      this.currencies$.next(currencies);
+      this.mainCurrency$.next(mainCurrency);
     })).toPromise();
+  }
+
+  setMainCurrency(currency: Currency) {
+    this.currencyHttpClient.setMainCurrency(currency)
+      .subscribe((currency) => this.mainCurrency$.next(currency));
   }
 }
