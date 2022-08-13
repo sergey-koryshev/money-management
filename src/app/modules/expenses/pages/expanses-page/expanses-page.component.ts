8b951c571fe58d99c0d2de422913d7ac1dd1f3c5
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-08-13 22:03:15 +0200

    added empty ExpansesPageComponent

---
+++ src/app/modules/expenses/pages/expanses-page/expanses-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expanses-page',
+  templateUrl: './expanses-page.component.html',
+  styleUrls: ['./expanses-page.component.scss']
+})
+export class ExpansesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
