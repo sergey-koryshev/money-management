14b384c765376498d1d5920752deffcf4acee1c3
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-08-13 15:44:06 +0200

    hardcoded user in navbar component

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { CurrencyService } from './../../services/currency.service';
 import { Component, OnInit } from '@angular/core';
 import { Currency } from '@models/currency.model';
+import { User } from '@models/user.model';
 
 @Component({
   selector: 'app-navbar',
@@ -11,10 +12,17 @@ export class NavbarComponent implements OnInit {
 
   currencies: Currency[];
   mainCurrency: Currency;
+  user: User;
 
   constructor(currencyService: CurrencyService) {
     this.currencies = currencyService.currencies;
     this.mainCurrency = currencyService.mainCurrency;
+    // TODO: need to remove this hardoced user once we implemented login process
+    this.user = {
+      id: '8eeb9d4b-d246-4075-a53a-fa31184f71ec',
+      firstName: 'Sergey',
+      secondName: 'Koryshev'
+    };
    }
 
   ngOnInit(): void { }
