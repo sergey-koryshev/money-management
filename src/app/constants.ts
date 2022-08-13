c53a49e1b5150ecdb9a8835b8e4d4812a9f8dcf3
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-08-13 17:06:32 +0200

    added const which represents empty main currency

---
+++ src/app/constants.ts
@@ -0,0 +1,8 @@
+import { Currency } from "@models/currency.model";
+
+export const emptyMainCurrency: Currency = {
+    id: 9999,
+    name: "Not selected",
+    friendlyName: "Main currency is not selected",
+    flagCode: "xx"
+}
