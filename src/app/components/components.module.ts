cd93529c209263f11847a6696305e6cb97a54209
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-08-14 23:27:23 +0200

    introduced component "PriceComponent"

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,20 +3,25 @@ import { CommonModule } from '@angular/common';
 import { CurrencyComponent } from './currency/currency.component';
 import { UserAvatarComponent } from './user-avatar/user-avatar.component';
 import { TableComponent } from './table/table.component';
+import { PriceComponent } from './price/price.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     CurrencyComponent,
     UserAvatarComponent,
-    TableComponent
+    TableComponent,
+    PriceComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   exports: [
     UserAvatarComponent,
     CurrencyComponent,
-    TableComponent
+    TableComponent,
+    PriceComponent
   ],
 })
 export class ComponentsModule { }
