76e1e084255cdb6b857a8ebe7d1996ee28628cd7
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-08-14 18:27:35 +0200

    removed hardcoded expenses and implemented logic to get them from resolver

--- src/app/modules/expenses/pages/expanses-page/expanses-page.component.ts
+++ src/app/modules/expenses/pages/expanses-page/expanses-page.component.ts
@@ -1,4 +1,6 @@
+import { Expense } from '@app/models/expense.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-expanses-page',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpansesPageComponent implements OnInit {
 
-  constructor() { }
+  expenses: Expense[];
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe((data) => this.expenses = data.expenses ?? []);
   }
-
 }
