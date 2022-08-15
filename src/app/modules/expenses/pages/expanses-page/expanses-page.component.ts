2b51d7b559aa09dcc3c8f398cc55fa042890ae77
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-08-15 18:10:45 +0200

    implemented logic to load new list of expenses when user changes main currency

--- src/app/modules/expenses/pages/expanses-page/expanses-page.component.ts
+++ src/app/modules/expenses/pages/expanses-page/expanses-page.component.ts
@@ -1,6 +1,9 @@
+import { ExpensesHttpClientService } from '@http-clients/expenses-http-client.service';
+import { CurrencyService } from '@services/currency.service';
 import { Expense } from '@app/models/expense.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, skip } from 'rxjs/operators';
 
 @Component({
   selector: 'app-expanses-page',
@@ -11,9 +14,17 @@ export class ExpansesPageComponent implements OnInit {
 
   expenses: Expense[];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private currencyService: CurrencyService, 
+    private expensesHttpClient: ExpensesHttpClientService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe((data) => this.expenses = data.expenses ?? []);
+    this.currencyService.mainCurrency$
+      .pipe(
+        skip(1),
+        switchMap(() => this.expensesHttpClient.getAllExpenses()))
+      .subscribe((expenses) => this.expenses = expenses);
   }
 }
