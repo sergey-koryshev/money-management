73d3d080093f69bd532823ba188604010292f0cb
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-08-17 21:27:03 +0200

    implemented sorting logic; added property "defaultSorting" to set default sorting for table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,7 +1,8 @@
-import { TableColumn, TableColumnType } from './table.model';
-import { Component, Input, OnInit } from '@angular/core';
+import { SortEvent, TableColumn, TableColumnType } from './table.model';
+import { Component, Input, QueryList, ViewChildren, OnInit } from '@angular/core';
 import { emptyTableData } from '@app/constants';
 import { ObjectKey } from '@app/models/base.model';
+import { SortableHeaderDirective } from './sortable-header.directive';
 
 @Component({
   selector: 'app-table',
@@ -9,19 +10,34 @@ import { ObjectKey } from '@app/models/base.model';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent<T> implements OnInit {
-
   @Input()
   columns: TableColumn<T>[];
 
   @Input()
   data: T[];
 
+  @Input()
+  defaultSorting: SortEvent;
+
+  sortedData: T[];
+
   columnTypes = TableColumnType;
+
   emptyTableData = emptyTableData;
 
-  constructor() { }
+  @ViewChildren(SortableHeaderDirective)
+  headers: QueryList<SortableHeaderDirective>;
 
   ngOnInit(): void {
+    this.sortedData = this.data;
+    this.setDefaultSorting();
+  }
+
+  setDefaultSorting() {
+    if (!this.defaultSorting) {
+      return;
+    }
+    this.onSort(this.defaultSorting, true);
   }
 
   getColumnType(column: TableColumn<T>): TableColumnType {
@@ -40,4 +56,32 @@ export class TableComponent<T> implements OnInit {
     const propertyKey = propertyName as ObjectKey;
     return object[propertyKey];
   }
+
+  onSort({column, direction}: SortEvent, skipHeaders: boolean = false) {
+    if (!skipHeaders) {
+      this.headers.forEach((header: SortableHeaderDirective) => {
+        if (header.sortable !== column) {
+          header.direction = '';
+        }
+      });
+    }
+    if (direction === '' || column === '') {
+      this.sortedData = this.data;
+    } else {
+      this.sortedData = [...this.data].sort((a, b) => {
+        const res = this.compareProperties(this.extractProperty(a, column), this.extractProperty(b, column));
+        return direction === 'asc' ? res : -res;
+      });
+    }
+  }
+
+  compareProperties(a: any, b: any) {
+    if ((typeof a === 'string') || (typeof a === 'number') && 
+      (typeof b === 'string' || typeof b === 'number')) {
+      return a < b ? -1 : a > b ? 1 : 0;
+    }
+
+    return 0;
+  }
 }
+
