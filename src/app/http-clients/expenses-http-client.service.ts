ea6b8ab39fe5148b043d44af0dd65472c4ce67ac
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-10-15 22:12:52 +0200

    changed params for adding new expense

--- src/app/http-clients/expenses-http-client.service.ts
+++ src/app/http-clients/expenses-http-client.service.ts
@@ -1,6 +1,7 @@
 import { Expense } from '@app/models/expense.model';
 import { Injectable } from '@angular/core';
 import { BaseHttpClientService } from './base-http-client.service';
+import { AddExpenseParams } from './expenses-http-client.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class ExpensesHttpClientService {
     return this.baseHttpClient.get<Expense[]>('expenses');
   }
 
-  addNewExpense(expense: Expense) {
+  addNewExpense(expense: AddExpenseParams) {
     return this.baseHttpClient.post<Expense>('expenses', expense);
   }
 }
