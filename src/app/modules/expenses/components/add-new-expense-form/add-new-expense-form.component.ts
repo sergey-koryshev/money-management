a1b4e12c5aa8b13986bbe0873f4f214ccfad9f17
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-10-16 00:04:04 +0200

    added logic to save last used currency and set it as default in add new expense form

--- src/app/modules/expenses/components/add-new-expense-form/add-new-expense-form.component.ts
+++ src/app/modules/expenses/components/add-new-expense-form/add-new-expense-form.component.ts
@@ -1,6 +1,6 @@
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 import { CurrencyService } from '@services/currency.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Currency } from '@app/models/currency.model';
 
@@ -9,23 +9,33 @@ import { Currency } from '@app/models/currency.model';
   templateUrl: './add-new-expense-form.component.html',
   styleUrls: ['./add-new-expense-form.component.scss']
 })
-export class AddNewExpenseComponent {
-
+export class AddNewExpenseComponent implements OnInit {
+  private defaultCurrencyIdStorageName = 'default-currency';
   currencies: Currency[]
-
   form = this.fb.group({
     'date': [this.currentDate, Validators.required],
     'item': [null, Validators.required],
     'priceAmount': [null, Validators.required],
-    'currencyId': [null, Validators.required]
-  })
+    'currencyId': [this.defaultCurrency, Validators.required]
+  });
+  get currentDate(): NgbDate {
+    const now = new Date();
+    return new NgbDate(now.getFullYear(), now.getMonth() + 1, now.getDate());
+  }
+  get defaultCurrency(): number {
+    const currencyId = localStorage.getItem(this.defaultCurrencyIdStorageName);
+    return currencyId ? Number(currencyId) : 0;
+  }
 
   constructor(private fb: FormBuilder, currency: CurrencyService) {
     this.currencies = currency.currencies;
   }
 
-  get currentDate(): NgbDate {
-    const now = new Date();
-    return new NgbDate(now.getFullYear(), now.getMonth() + 1, now.getDate());
+  ngOnInit(): void {
+    this.form.get('currencyId')?.valueChanges.subscribe((value) => {
+      if (value) {
+        localStorage.setItem(this.defaultCurrencyIdStorageName, String(value));
+      }
+    });
   }
 }
