75153cb868307fc16807296f3413c900db8022d8
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-06-27 07:49:30 +0200

    corrected expenses resolver to pass month for function to get expenses

--- src/app/modules/expenses/expenses.resolver.ts
+++ src/app/modules/expenses/expenses.resolver.ts
@@ -1,19 +1,19 @@
 import { ExpensesHttpClientService } from './../../http-clients/expenses-http-client.service';
 import { Injectable } from '@angular/core';
-import {
-  Router, Resolve,
+import { Resolve,
   RouterStateSnapshot,
   ActivatedRouteSnapshot
 } from '@angular/router';
 import { Expense } from '@app/models/expense.model';
-import { Observable, of } from 'rxjs';
+import { ExpensesMonthService } from '@app/services/expenses-month.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpensesResolver implements Resolve<Expense[]> {
-  constructor(private expensesHttpClient: ExpensesHttpClientService) {}
+  constructor(private expensesHttpClient: ExpensesHttpClientService, private expensesMonthService: ExpensesMonthService) {}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Expense[]>  {
-    return this.expensesHttpClient.getAllExpenses();
+    return this.expensesHttpClient.getAllExpenses(this.expensesMonthService.month);
   }
 }
