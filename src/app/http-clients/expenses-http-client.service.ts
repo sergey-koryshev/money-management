4053f41b8f4cc594b77c735bcbef973b4eba4a1c
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-06-27 07:50:25 +0200

    added parameter selectedMonth to function which gets all expenses

--- src/app/http-clients/expenses-http-client.service.ts
+++ src/app/http-clients/expenses-http-client.service.ts
@@ -2,6 +2,7 @@ import { Expense } from '@app/models/expense.model';
 import { Injectable } from '@angular/core';
 import { BaseHttpClientService } from './base-http-client.service';
 import { AddExpenseParams } from './expenses-http-client.model';
+import { Month } from '@app/models/month.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,11 @@ export class ExpensesHttpClientService {
 
   constructor(private baseHttpClient: BaseHttpClientService) { }
 
-  getAllExpenses() {
-    return this.baseHttpClient.get<Expense[]>('expenses');
+  getAllExpenses(selectedMonth: Month) {
+    return this.baseHttpClient.get<Expense[]>('expenses', {
+      "month": selectedMonth.month,
+      "year": selectedMonth.year
+    });
   }
 
   addNewExpense(expense: AddExpenseParams) {
