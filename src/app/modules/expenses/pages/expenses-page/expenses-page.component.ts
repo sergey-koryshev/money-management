8163325858f65347f9accd8b062894596b34080f
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-06-27 08:11:11 +0200

    Ability to change month (#13)
    ## Description of fix
    
    - added new service to store month. `Month` model contains `month` and
    `year`
    - implemented date picker to select month at the top of the page; button
    to add new expense was moved to right
    - corrected `Mock Server`'s logic to be able to filter expenses by month
    and year

--- src/app/modules/expenses/pages/expenses-page/expenses-page.component.ts
+++ src/app/modules/expenses/pages/expenses-page/expenses-page.component.ts
@@ -1,40 +1,56 @@
+import { ExpensesMonthService } from './../../../../services/expenses-month.service';
 import { ExpensesHttpClientService } from '@http-clients/expenses-http-client.service';
 import { CurrencyService } from '@services/currency.service';
 import { Expense } from '@app/models/expense.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap, skip } from 'rxjs/operators';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDate, NgbDatepickerNavigateEvent, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddExpenseParams } from '@app/http-clients/expenses-http-client.model';
+import { Month } from '@app/models/month.model';
 
 @Component({
   selector: 'app-expenses-page',
-  templateUrl: './expenses-page.component.html'
+  templateUrl: './expenses-page.component.html',
+  styleUrls: ['./expenses-page.component.scss']
 })
 export class ExpensesPageComponent implements OnInit {
 
   expenses: Expense[];
+  selectedMonth: Month;
 
   constructor(
-    private route: ActivatedRoute, 
-    private currencyService: CurrencyService, 
+    private route: ActivatedRoute,
+    private currencyService: CurrencyService,
     private expensesHttpClient: ExpensesHttpClientService,
-    private modalService: NgbModal) { }
+    private modalService: NgbModal,
+    private expensesMonthService: ExpensesMonthService) { }
 
   ngOnInit(): void {
+    this.selectedMonth = this.expensesMonthService.getMonth();
     this.route.data.subscribe((data) => this.expenses = data.expenses ?? []);
     this.currencyService.mainCurrency$
       .pipe(
-        //we need to skip first item emitted by BehaviorSubject because we already get initial data from route resolver 
+        //we need to skip first item emitted by BehaviorSubject because we already get initial data from route resolver
         skip(1),
-        switchMap(() => this.expensesHttpClient.getAllExpenses()))
+        switchMap(() => this.expensesHttpClient.getAllExpenses(this.expensesMonthService.month)))
+      .subscribe((expenses) => this.expenses = expenses);
+    this.expensesMonthService.month$
+      .pipe(
+        //we need to skip first item emitted by BehaviorSubject because we already get initial data from route resolver
+        skip(1),
+        switchMap(() => this.expensesHttpClient.getAllExpenses(this.expensesMonthService.month)))
       .subscribe((expenses) => this.expenses = expenses);
   }
 
   addNewExpense(expense: AddExpenseParams) {
     this.expensesHttpClient.addNewExpense(expense)
       .subscribe((addedExpanse) => {
-        this.expenses.push(addedExpanse);
+        const date = new Date(addedExpanse.date)
+        if (this.expensesMonthService.month.month == date.getMonth() + 1
+          && this.expensesMonthService.month.year == date.getFullYear()) {
+            this.expenses.push(addedExpanse);
+          }
       });
   }
 
@@ -45,6 +61,13 @@ export class ExpensesPageComponent implements OnInit {
         date: new Date(date.year, date.month - 1, date.day),
         ...restParams
       });
+    });
+  }
+
+  onMonthChange(value: NgbDatepickerNavigateEvent) {
+    this.expensesMonthService.month$.next({
+      month: value.next.month,
+      year: value.next.year
     });
   }
 }
