0351d4377243b25e5b01de1adff02a74fb811fce
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-06-27 07:48:38 +0200

    added service to store selected month

---
+++ src/app/services/expenses-month.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, forkJoin } from 'rxjs';
+import { Month } from '@app/models/month.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpensesMonthService {
+  selectedMonthStorageName = "selected-month";
+  month$: BehaviorSubject<Month>;
+  get month() {
+    return this.month$.value;
+  }
+
+  constructor() {
+    this.month$ = new BehaviorSubject<Month>(this.getMonth());
+    this.month$.subscribe((value) => this.saveMonth(value));
+  }
+
+  getMonth(): Month {
+    const selectedMonth = localStorage.getItem(this.selectedMonthStorageName);
+    const currentDate = new Date();
+    return selectedMonth ? JSON.parse(selectedMonth) : {
+      month: currentDate.getMonth(),
+      year: currentDate.getFullYear()
+    };
+  }
+
+  saveMonth(selectedMonth: Month) {
+    localStorage.setItem(this.selectedMonthStorageName, JSON.stringify(selectedMonth));
+  }
+}
