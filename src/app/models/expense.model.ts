3ded193c761308577814ecc02b7d69ecb86a10fa
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-09-03 15:57:15 +0200

    added Category model

--- src/app/models/expense.model.ts
+++ src/app/models/expense.model.ts
@@ -1,9 +1,11 @@
+import { Category } from "./category.model";
 import { ExchangedPrice } from "./exchanged-price.model";
 import { Price } from "./price.model";
 
 export interface Expense {
     id?: number;
     date: Date;
+    category: Category
     item: string;
     price: Price;
     exchangedPrice?: ExchangedPrice;
