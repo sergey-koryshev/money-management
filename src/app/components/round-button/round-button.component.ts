67cc2e0cbf28f61eb69c4e33583d40eb29bbac08
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-10-01 12:45:06 +0200

    added component for round button

---
+++ src/app/components/round-button/round-button.component.ts
@@ -0,0 +1,29 @@
+import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Shade } from '@app/helpers/colors.helper';
+
+@Component({
+  selector: 'app-round-button',
+  templateUrl: './round-button.component.html',
+  styleUrls: ['./round-button.component.scss']
+})
+export class RoundButtonComponent implements AfterViewInit {
+
+  @Input()
+  backgroundColor: string;
+
+  currentBackgroundColor: string;
+  lighterBackgroundColor: string;
+
+  ngAfterViewInit(): void {
+    this.currentBackgroundColor = this.backgroundColor;
+    this.lighterBackgroundColor = Shade(this.backgroundColor, -10);
+  }
+
+  onMouseOver() {
+    this.currentBackgroundColor = this.lighterBackgroundColor;
+  }
+
+  onMouseLeave() {
+    this.currentBackgroundColor = this.backgroundColor;
+  }
+}
