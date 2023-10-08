9b3f2eb6b800d56371703ae656e001308312ffd0
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-10-08 10:53:34 +0200

    implemented logic to change currency when main one is changed

--- src/app/modules/expenses/pages/search-results/search-results.component.ts
+++ src/app/modules/expenses/pages/search-results/search-results.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ExpensesHttpClientService } from '@app/http-clients/expenses-http-client.service';
 import { Expense } from '@app/models/expense.model';
+import { CurrencyService } from '@app/services/currency.service';
+import { skip, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-search-results',
@@ -10,10 +13,16 @@ export class SearchResultsComponent implements OnInit {
 
   expenses: Expense[];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private currencyService: CurrencyService, private expensesHttpClient: ExpensesHttpClientService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe((data) => this.expenses = data.expenses ?? []);
+    this.currencyService.mainCurrency$
+      .pipe(
+        //we need to skip first item emitted by BehaviorSubject because we already get initial data from route resolver
+        skip(1),
+        switchMap(() => this.expensesHttpClient.searchExpenses(this.route.snapshot.queryParams['text'])))
+      .subscribe((expenses) => this.expenses = expenses);
   }
 
 }
