91e1092a03befe8bf75d11e72939ac33dbb9da7c
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-10-16 08:25:53 +0200

    modified expenses controller to operate with expenses view instead of expenses list

--- src/app/modules/expenses/expenses.module.ts
+++ src/app/modules/expenses/expenses.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
     path: '',
     component: ExpensesPageComponent,
     resolve: {
-      expenses: ExpensesResolver,
+      expensesView: ExpensesResolver,
     }
   },
   {
--- src/app/modules/expenses/expenses.resolver.ts
+++ src/app/modules/expenses/expenses.resolver.ts
@@ -4,16 +4,16 @@ import { Resolve,
   RouterStateSnapshot,
   ActivatedRouteSnapshot
 } from '@angular/router';
-import { Expense } from '@app/models/expense.model';
+import { ExpensesView } from '@app/models/expenses-view.model';
 import { ExpensesMonthService } from '@app/services/expenses-month.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ExpensesResolver implements Resolve<Expense[]> {
+export class ExpensesResolver implements Resolve<ExpensesView> {
   constructor(private expensesHttpClient: ExpensesHttpClientService, private expensesMonthService: ExpensesMonthService) {}
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Expense[]>  {
-    return this.expensesHttpClient.getAllExpenses(this.expensesMonthService.month);
+  resolve(): Observable<ExpensesView>  {
+    return this.expensesHttpClient.getExpensesView(this.expensesMonthService.month);
   }
 }
