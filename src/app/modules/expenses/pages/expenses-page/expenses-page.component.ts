7305d57503e55fd95340c55b775474d53410842d
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-10-16 08:28:27 +0200

    added logic to correct total spent counter if any item in expenses table is changed; small refactoring

--- src/app/modules/expenses/pages/expenses-page/expenses-page.component.ts
+++ src/app/modules/expenses/pages/expenses-page/expenses-page.component.ts
@@ -1,24 +1,29 @@
-import { ExpensesMonthService } from './../../../../services/expenses-month.service';
+import { ExpensesMonthService } from '@services/expenses-month.service';
 import { ExpensesHttpClientService } from '@http-clients/expenses-http-client.service';
 import { CurrencyService } from '@services/currency.service';
 import { Expense } from '@app/models/expense.model';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap, skip } from 'rxjs/operators';
 import { NgbDatepickerNavigateEvent, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddExpenseParams } from '@app/http-clients/expenses-http-client.model';
 import { Month } from '@app/models/month.model';
 import { AddNewExpenseDialogComponent } from '../../components/add-new-expense-dialog/add-new-expense-dialog.component';
+import { Price } from '@app/models/price.model';
+import { ExpensesView } from '@app/models/expenses-view.model';
+import { ItemChangedEventArgs } from '../../components/expenses-table/expenses-table.model';
+import { forkJoin, merge, zip } from 'rxjs';
 
 @Component({
   selector: 'app-expenses-page',
   templateUrl: './expenses-page.component.html',
   styleUrls: ['./expenses-page.component.scss']
 })
-export class ExpensesPageComponent implements OnInit {
+export class ExpensesPageComponent implements OnInit, AfterViewInit {
 
   expenses: Expense[];
   selectedMonth: Month;
+  totalAmount?: Price;
 
   constructor(
     private route: ActivatedRoute,
@@ -28,20 +33,21 @@ export class ExpensesPageComponent implements OnInit {
     private expensesMonthService: ExpensesMonthService) { }
 
   ngOnInit(): void {
-    this.selectedMonth = this.expensesMonthService.getMonth();
-    this.route.data.subscribe((data) => this.expenses = data.expenses ?? []);
+    this.selectedMonth = this.expensesMonthService.month;
+    this.route.data.subscribe(data => this.populateData(data.expensesView as ExpensesView));
+  }
+
+  ngAfterViewInit(): void {
     this.currencyService.mainCurrency$
       .pipe(
-        //we need to skip first item emitted by BehaviorSubject because we already get initial data from route resolver
         skip(1),
-        switchMap(() => this.expensesHttpClient.getAllExpenses(this.expensesMonthService.month)))
-      .subscribe((expenses) => this.expenses = expenses);
+        switchMap(() => this.expensesHttpClient.getExpensesView(this.expensesMonthService.month)))
+      .subscribe(data => this.populateData(data));
     this.expensesMonthService.month$
       .pipe(
-        //we need to skip first item emitted by BehaviorSubject because we already get initial data from route resolver
         skip(1),
-        switchMap(() => this.expensesHttpClient.getAllExpenses(this.expensesMonthService.month)))
-      .subscribe((expenses) => this.expenses = expenses);
+        switchMap(() => this.expensesHttpClient.getExpensesView(this.expensesMonthService.month)))
+      .subscribe(data => this.populateData(data));
   }
 
   addNewExpense(expense: AddExpenseParams) {
@@ -51,6 +57,9 @@ export class ExpensesPageComponent implements OnInit {
         if (this.expensesMonthService.month.month == date.getMonth() + 1
           && this.expensesMonthService.month.year == date.getFullYear()) {
             this.expenses.push(addedExpanse);
+            this.onItemChange({
+              newValue: addedExpanse.exchangedPrice?.amount ?? addedExpanse.price.amount
+            })
           }
       });
   }
@@ -71,5 +80,25 @@ export class ExpensesPageComponent implements OnInit {
       month: value.next.month,
       year: value.next.year
     });
+    this.selectedMonth = this.expensesMonthService.month;
+  }
+
+  onItemChange(args: ItemChangedEventArgs) {
+    if (!this.totalAmount) {
+      return;
+    }
+
+    if (args.oldValue) {
+      this.totalAmount.amount = this.totalAmount.amount - args.oldValue;
+    }
+
+    if (args.newValue) {
+      this.totalAmount.amount = this.totalAmount.amount + args.newValue;
+    }
+  }
+
+  private populateData(view: ExpensesView) {
+    this.expenses = view.expenses ?? [];
+    this.totalAmount = view.total;
   }
 }
