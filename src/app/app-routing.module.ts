c787ad9b7c66e09b5acaeeccd943bf3a6d5d4816
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-10-19 08:34:37 +0200

    added login module with route /login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path: 'expenses',
-  loadChildren: () => import('./modules/expenses/expenses.module').then((module) => module.ExpensesModule)
-}];
+const routes: Routes = [
+  {
+    path: 'expenses',
+    loadChildren: () => import('./modules/expenses/expenses.module').then((module) => module.ExpensesModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./modules/login/login.module').then((module) => module.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
