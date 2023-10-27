ff7dac12c8eaa9119c42fb7075e77c633467584e
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-10-27 09:06:49 +0200

    added users to data context

---
+++ mock-server/src/data/users.data.ts
@@ -0,0 +1,12 @@
+import { User } from '../models/user.model';
+
+export const users: User[] = [
+  {
+    id: 1,
+    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c',
+    email: 'test@test.com',
+    firstName: 'Sergey',
+    secondName: 'Koryshev',
+    password: '$2b$10$7RmSyFQRVeT/6Z93E6ZOqOds8REBHn7YRW/fRSzetEbvuQyNlfwUy'
+  }
+]
