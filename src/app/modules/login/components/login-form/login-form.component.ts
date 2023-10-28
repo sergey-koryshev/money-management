96810ae3609373799f4b474709f1d2265a11767f
Sergey Koryshev <sergey.koryshev@outlook.com>
2023-10-28 17:49:49 +0200

    added logic for LoginForm component to perform login action

--- src/app/modules/login/components/login-form/login-form.component.ts
+++ src/app/modules/login/components/login-form/login-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginHttpClient } from '@app/http-clients/login-http-client.service';
+import { AuthService } from '@app/services/auth.service';
+import { CurrencyService } from '@app/services/currency.service';
 
 @Component({
   selector: 'app-login-form',
@@ -9,13 +13,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginFormComponent implements OnInit {
 
   form: FormGroup;
+  error: string | null;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private authService: AuthService, private loginHttpClient: LoginHttpClient, private router: Router, private currencyService: CurrencyService) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
       'email': [null, Validators.required],
       'password': [null, Validators.required]
     });
+  }
+
+  onSubmit() {
+    const {email, password} = this.form.value;
+    this.loginHttpClient.login(email, password).subscribe({
+      next: (user) => {
+        this.error = null;
+        this.authService.saveUser(user);
+        this.router.navigate(['expenses']);
+      },
+      error: (err) => this.error = err.error.data ?? err.error.message})
   }
 }
