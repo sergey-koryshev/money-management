861ebf0854d86324819ad718c553562dd3583a51
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-01-12 08:06:59 +0100

    modified mock data to make it be stored like in DB

---
+++ mock-server/src/data/main-currencies.data.ts
@@ -0,0 +1,14 @@
+import { MainCurrencyEntity } from './entities/main-currency.entity';
+
+export const mainCurrencies: MainCurrencyEntity[] = [
+  {
+    id: 0,
+    currencyId: 1,
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada',
+  },
+  {
+    id: 1,
+    currencyId: 3,
+    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c',
+  }
+]
--- mock-server/src/data/main-currency.data.ts
+++
@@ -1,9 +0,0 @@
-import { Currency } from '../models/currency.model';
-
-export const mainCurrency: Currency = {
-  id: 3,
-  name: 'EUR',
-  friendlyName: 'Euro',
-  flagCode: 'eu',
-  sign: '€'
-}
