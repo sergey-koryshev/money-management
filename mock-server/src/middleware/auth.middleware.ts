715c67a1d9807703239529093a269657aba5bbb0
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-01-12 08:08:46 +0100

    added logic to populate user tenant to controllers

--- mock-server/src/middleware/auth.middleware.ts
+++ mock-server/src/middleware/auth.middleware.ts
@@ -1,5 +1,6 @@
 import * as jwt from 'jsonwebtoken'
 import { NextFunction, Request, Response } from 'express';
+import { JwtToken } from '../models/jwt-token.model';
 
 export function auth(req: Request, res: Response, next: NextFunction) {
   const accessToken = req?.cookies['access_token'];
@@ -8,12 +9,16 @@ export function auth(req: Request, res: Response, next: NextFunction) {
     return res.sendStatus(401);
   }
 
-  jwt.verify(accessToken, process.env.AUTH_TOKEN_SECRET as string, (err: unknown) => {
+  jwt.verify(accessToken, process.env.AUTH_TOKEN_SECRET as string, (err: jwt.VerifyErrors | null, decoded?: jwt.JwtPayload | string) => {
     if (err) {
       console.log(err);
       res.clearCookie('access_token');
       return res.sendStatus(401);
     }
+
+    const { tenant } = decoded as JwtToken
+    req.userTenant = tenant;
+
     next();
   });
 }
