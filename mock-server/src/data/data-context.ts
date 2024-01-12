fa6d3c412f59910f3ec11609fb0e0f5aa7a181ec
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-01-12 08:07:30 +0100

    modified class DataContext to reflect changes in mock data

--- mock-server/src/data/data-context.ts
+++ mock-server/src/data/data-context.ts
@@ -1,128 +1,45 @@
-import { Category } from '../models/category.model';
-import { Currency } from '../models/currency.model';
-import { ExchangedPrice } from '../models/exchanged-price.model';
-import { Expense } from '../models/expense.model';
-import { User } from '../models/user.model';
-import { categories } from './categories.data';
+import { categories, categoryEntityToModel } from './categories.data';
+import { expenseEntityToModel, expenses } from './expenses.data';
+import { Identifier } from '../models/identified.model';
 import { currencies } from './currencies.data';
-import { expenses } from './expenses.data';
-import { mainCurrency } from './main-currency.data';
+import { mainCurrencies } from './main-currencies.data';
 import { users } from './users.data';
 
-interface ExchangeRate {
-  fromCurrencyId: number
-  toCurrencyId: number
-  rate: number
-}
-
 export class DataContext {
-  public currencies: Currency[];
-  public mainCurrency: Currency | null;
-  public expenses: Expense[];
-  public exchangedExpenses: Expense[];
-  public categories: Category[];
-  public users: User[];
+  public expensesDbSet = expenses;
+  public categoriesDbSet = categories;
+  public mainCurrenciesDbSet = mainCurrencies;
 
-  private exchangeRates: ExchangeRate[] = [
-    {
-      fromCurrencyId: 0,
-      toCurrencyId: 1,
-      rate: 1.29
-    },
-    {
-      fromCurrencyId: 0,
-      toCurrencyId: 2,
-      rate: 0.012
-    },
-    {
-      fromCurrencyId: 0,
-      toCurrencyId: 3,
-      rate: 0.011
-    },
-    {
-      fromCurrencyId: 1,
-      toCurrencyId: 0,
-      rate: 0.78
-    },
-    {
-      fromCurrencyId: 1,
-      toCurrencyId: 2,
-      rate: 0.0093
-    },
-    {
-      fromCurrencyId: 1,
-      toCurrencyId: 3,
-      rate: 0.0085
-    },
-    {
-      fromCurrencyId: 2,
-      toCurrencyId: 0,
-      rate: 83.40
-    },
-    {
-      fromCurrencyId: 2,
-      toCurrencyId: 1,
-      rate: 107.55
-    },
-    {
-      fromCurrencyId: 2,
-      toCurrencyId: 3,
-      rate: 0.92
-    },
-    {
-      fromCurrencyId: 3,
-      toCurrencyId: 0,
-      rate: 91.06
-    },
-    {
-      fromCurrencyId: 3,
-      toCurrencyId: 1,
-      rate: 117.42
-    },
-    {
-      fromCurrencyId: 3,
-      toCurrencyId: 2,
-      rate: 1.09
-    }
-  ];
+  public currenciesDbSet = currencies;
+  public usersDbSet = users;
 
-  constructor() {
-    this.currencies = currencies;
-    this.mainCurrency = mainCurrency;
-    this.expenses = expenses;
-    this.exchangedExpenses = [];
-    this.categories = categories;
-    this.users = users;
-    this.recalculateExchangedExpenses();
+  public getExpenses(tenant: string) {
+    return this.expensesDbSet.filter((e) => e.tenant === tenant).map(expenseEntityToModel);
   }
 
-  public recalculateExchangedExpenses(){
-    this.exchangedExpenses = this.exchangeExpenses();
+  public getCategories(tenant: string) {
+    return this.categoriesDbSet.filter((e) => e.tenant === tenant).map(categoryEntityToModel);
   }
 
-  private exchangeExpenses(): Expense[] {
-    return this.expenses.map((e) => {
-      const exchangeRate = this.exchangeRates.find((r) =>
-        r.fromCurrencyId === e.price.currency.id &&
-        r.toCurrencyId === this.mainCurrency?.id);
+  public getMainCurrency(tenant: string) {
+    return this.mainCurrenciesDbSet.find((m) => m.tenant === tenant);
+  }
 
-      let exchangedPrice: ExchangedPrice | undefined = undefined;
+  public addEntity<T extends Identifier>(entity: T, dbSet: T[]) {
+    const newId = this.getUniqueId(dbSet);
+    const addingEntity = {...entity, id: newId};
+    dbSet.push(addingEntity);
+    return addingEntity;
+  }
 
-      if(exchangeRate) {
-        exchangedPrice = {
-          amount: e.price.amount * exchangeRate.rate,
-          currency: this.currencies.find((c) => c.id === exchangeRate.toCurrencyId) ?? this.currencies[0],
-          exchangeDate: e.date,
-          exchangeRate: exchangeRate.rate,
-          originalAmount: e.price.amount,
-          originalCurrency: e.price.currency
-        }
-      }
-
-      return {
-        ...e,
-        exchangedPrice
-      }
-    })
+  private getUniqueId<T extends Identifier>(dbSet: T[]){
+    return dbSet.reduce(
+      (max, e) => (e.id
+        ? e.id > max
+          ? e.id
+          : max
+        : 0),
+      0
+    ) + 1;
   }
 }
