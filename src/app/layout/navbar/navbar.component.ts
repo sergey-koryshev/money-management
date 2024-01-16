d740e83066707e07d1e80e953de359be89c79361
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-01-16 11:42:59 +0100

    renamed dropdown item "Change password" to "Connections"; implemented logic to display pending connections counter at the right of the dropdown item

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '@app/services/auth.service';
 import { LoginHttpClient } from '@app/http-clients/login-http-client.service';
+import { UserConnectionStatus } from '@app/models/enums/user-connection-status.enum';
 
 @Component({
   selector: 'app-navbar',
@@ -20,19 +21,26 @@ export class NavbarComponent {
   user: User | null;
   emptyMainCurrency = emptyMainCurrency;
   searchForm: FormGroup;
+  pendingConnections: number = 0;
 
   get defaultUser(): User {
     return {
       id: 0,
       tenant: '8eeb9d4b-d246-4075-a53a-fa31184f71ec',
-      firstName: 'Sign In'
+      firstName: 'Sign In',
+      connections: []
     };
   }
 
   constructor(private currencyService: CurrencyService, private router: Router, fb: FormBuilder, private authService: AuthService, private loginHttpClient: LoginHttpClient) {
     this.currencyService.currencies$.subscribe((currencies) => this.currencies = currencies);
     this.currencyService.mainCurrency$.subscribe((currency) => this.mainCurrency = currency)
-    authService.user$.subscribe((user) => this.user = user)
+    authService.user$.subscribe((user) => {
+      this.user = user;
+      if (user) {
+        this.pendingConnections = user.connections.filter((c) => c.status === UserConnectionStatus.pending).length;
+      }
+    });
     this.searchForm = fb.group({
       text: ['', Validators.required]
     });
