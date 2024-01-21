28e432f7ecfd59a046f062cb8e86dc6e89328351
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-01-21 10:43:12 +0100

    implemented methods to call user connection related endpoints

--- src/app/http-clients/user-connections-http-client.service.ts
+++ src/app/http-clients/user-connections-http-client.service.ts
@@ -1,14 +1,35 @@
+import { CreateUserConnectionParams } from './../../../mock-server/src/models/create-user-connection-params.model';
 import { Injectable } from '@angular/core';
 import { BaseHttpClientService } from './base-http-client.service';
+import { UserConnection } from '@app/models/user-connnection.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserConnectionHttpClient {
-
   constructor(private baseHttpClient: BaseHttpClientService) { }
 
   getPendingConnectionsCount() {
-    return this.baseHttpClient.get<number>('userConnections/getPendingConnectionsCount')
+    return this.baseHttpClient.get<number>('userConnections/pendingConnectionsCount');
+  }
+
+  getUserConnections() {
+    return this.baseHttpClient.get<UserConnection[]>('userConnections');
+  }
+
+  acceptUserConnections(connectionId: number) {
+    return this.baseHttpClient.post<UserConnection>(`userConnections/${connectionId}/accept`);
+  }
+
+  declineUserConnections(connectionId: number) {
+    return this.baseHttpClient.post<void>(`userConnections/${connectionId}/decline`);
+  }
+
+  deleteUserConnections(connectionId: number) {
+    return this.baseHttpClient.delete<void>(`userConnections/${connectionId}`);
+  }
+
+  createUserConnection(params: CreateUserConnectionParams) {
+    return this.baseHttpClient.post<UserConnection>(`userConnections`, params)
   }
 }
