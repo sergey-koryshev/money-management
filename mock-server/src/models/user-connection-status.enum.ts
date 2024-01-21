18defa0164e4cf3449d503b06b0ca68a9ebb127a
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-01-21 10:42:41 +0100

    implemented all required endpoints to work with user connections

--- mock-server/src/models/user-connection-status.enum.ts
+++ mock-server/src/models/user-connection-status.enum.ts
@@ -1,5 +1,5 @@
 export enum UserConnectionStatus {
-  accepted,
   pending,
-  pendingOnTargetUser
+  pendingOnTargetUser,
+  accepted
 }
--- mock-server/src/routers/user-connections.router.ts
+++ mock-server/src/routers/user-connections.router.ts
@@ -5,7 +5,12 @@ import { auth } from '../middleware/auth.middleware';
 export class UserConnectionRouter extends RouterBase<UserConnectionsController> {
   initialize(): void {
     this.router.use(auth);
-    this.router.get('/getPendingConnectionsCount', this.controller.getPendingConnectionsCount);
+    this.router.get('/pendingConnectionsCount', this.controller.getPendingConnectionsCount);
+    this.router.get('', this.controller.getUserConnections);
+    this.router.post('/:id/accept', this.controller.acceptRequest);
+    this.router.post('/:id/decline', this.controller.declineRequest);
+    this.router.delete('/:id', this.controller.deleteUserConnection);
+    this.router.post('', this.controller.createConnection);
   }
 }
 
