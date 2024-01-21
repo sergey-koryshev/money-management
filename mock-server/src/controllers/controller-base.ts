668657ac56ba36899afe98f554d72fa1a6550df9
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-01-21 10:35:32 +0100

    implemented methods sendError and sendData instead of wrapData

--- mock-server/src/controllers/controller-base.ts
+++ mock-server/src/controllers/controller-base.ts
@@ -1,11 +1,22 @@
 import { DataContext } from '../data/data-context';
+import { Response } from 'express';
 
 export abstract class ControllerBase {
   constructor(protected dataContext: DataContext) {}
 
-  protected wrapData<T>(data: T) {
-    return {
+  protected sendError(res: Response, statusCode: number, errorMassage: string) {
+    res.statusCode = statusCode;
+    return res.send({
+      code: statusCode,
+      message: errorMassage
+    })
+  }
+
+  protected sendData<T>(res: Response, data: T) {
+    res.statusCode = data ? 200 : 204;
+    return res.send({
+      code: 200,
       data
-    }
+    })
   }
 }
