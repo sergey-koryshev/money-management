76dbb8e245b5fabda887848d6e2136d3e0aebba6
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-01-27 17:11:16 +0100

    corrected angular's expense model

--- src/app/models/expense.model.ts
+++ src/app/models/expense.model.ts
@@ -1,14 +1,15 @@
 import { Category } from "./category.model";
 import { ExchangedPrice } from "./exchanged-price.model";
 import { Price } from "./price.model";
-import { User } from "./user.model";
+import { PolyUser } from "./user.model";
 
 export interface Expense {
-    id?: number
-    date: Date
-    category: Category
-    item: string
-    price: Price
-    exchangedPrice?: ExchangedPrice
-    sharedWith: User[]
+  id?: number
+  date: Date
+  category: Category
+  item: string
+  price: Price
+  exchangedPrice?: ExchangedPrice
+  sharedWith: PolyUser[]
+  isShared: boolean
 }
