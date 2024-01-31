6554f6e08e793f7f82af8cb3c0b20336ecc07470
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-01-31 08:49:56 +0100

    added demo data

--- mock-server/src/data/categories.data.ts
+++ mock-server/src/data/categories.data.ts
@@ -10,28 +10,98 @@ export function categoryEntityToModel(entity: CategoryEntity): Category {
 
 export const categories: CategoryEntity[] = [
   {
-    id: 0,
-    name: 'Restaurants (User 1)',
-    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
-  },
-  {
     id: 1,
-    name: 'Groceries (User 1)',
+    name: 'Online Shop',
     tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
   },
   {
     id: 2,
-    name: 'Web Shops (User 1)',
-    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
+    name: 'Bakery',
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
   },
   {
     id: 3,
-    name: 'Markets (User 2)',
+    name: 'Medicine',
     tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
   },
   {
     id: 4,
-    name: 'Delivery Food (User 2)',
+    name: 'Grocery',
+    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
+  },
+  {
+    id: 5,
+    name: 'Delivery Food',
     tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
+  },
+  {
+    id: 6,
+    name: 'Market',
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
+  },
+  {
+    id: 7,
+    name: 'Other',
+    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
+  },
+  {
+    id: 8,
+    name: 'Restaurant',
+    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
+  },
+  {
+    id: 9,
+    name: 'Coffee Shop',
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
+  },
+  {
+    id: 10,
+    name: 'Wine Shop',
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
+  },
+  {
+    id: 11,
+    name: 'Health & Beauty',
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
+  },
+  {
+    id: 12,
+    name: 'Clothing Shop',
+    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
+  },
+  {
+    id: 13,
+    name: 'Mobile & Internet',
+    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
+  },
+  {
+    id: 14,
+    name: 'Сonfectionery',
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
+  },
+  {
+    id: 15,
+    name: 'Rent',
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
+  },
+  {
+    id: 16,
+    name: 'Travel',
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
+  },
+  {
+    id: 17,
+    name: 'Fast Food',
+    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
+  },
+  {
+    id: 18,
+    name: 'Electronics Shop',
+    tenant: 'f1d4515b-f201-4696-86b8-3580ad740ada'
+  },
+  {
+    id: 19,
+    name: 'Tea Shop',
+    tenant: '22a11263-56da-4327-98b7-f99d6591ac3c'
   }
 ]
