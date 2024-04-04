734570c2452329d14ab7e19b7152006ab3af1d52
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-04-04 07:48:03 +0200

    implemented webpack

---
+++ mock-server/webpack.config.ts
@@ -0,0 +1,26 @@
+import * as path from 'path';
+import { Configuration } from 'webpack';
+
+const config: Configuration = {
+  entry: './src/index.ts',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true
+  }
+}
+
+export default config;
