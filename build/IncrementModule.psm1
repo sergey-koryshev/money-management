d21a3607def43c43d4ec73bafc1382efeab70973
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-04-13 11:13:50 +0200

    added custom logic to increment version in WebApi project

---
+++ build/IncrementModule.psm1
@@ -0,0 +1,56 @@
+$script:webApiVersionFileName = "Directory.Build.props"
+$script:versionXPath = "/Project/PropertyGroup/Version"
+
+function Get-Version {
+  [CmdletBinding()]
+  [OutputType([string])]
+  param ()
+
+  process {
+    $versionFilePath = Join-Path (Get-Location) $script:webApiVersionFileName
+
+    if (-not(Test-Path $versionFilePath)) {
+      throw "Version file '$versionFilePath' doesn't exist"
+    }
+
+    [xml]$versionFile = Get-Content -Path $versionFilePath
+    $versionNode = $versionFile.SelectSingleNode($script:versionXPath)
+
+    if ($null -eq $versionNode) {
+      throw "Version node cannot be found in file '$versionFilePath'"
+    }
+
+    Write-Output $versionNode.InnerText
+  }
+}
+
+function Set-Version {
+  [CmdletBinding()]
+  param (
+    [string]
+    $OldVersion,
+
+    [string]
+    $NewVersion
+  )
+  
+  process {
+    $versionFilePath = Join-Path (Get-Location) $script:webApiVersionFileName
+
+    if (-not(Test-Path $versionFilePath)) {
+      throw "Version file '$versionFilePath' doesn't exist"
+    }
+
+    [xml]$versionFile = Get-Content -Path $versionFilePath
+    $versionNode = $versionFile.SelectSingleNode($script:versionXPath)
+
+    if ($null -eq $versionNode) {
+      throw "Version node cannot be found in file '$versionFilePath'"
+    }
+
+    $versionNode.InnerText = $NewVersion
+    $versionFile.Save($versionFilePath)
+  }
+}
+
+Export-ModuleMember -Function @('Get-Version', 'Set-Version')
