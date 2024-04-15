08c50a4939f2c219c1135fe0b0a4a8324db974ab
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-04-15 20:44:24 +0200

    Added DbContext class

---
+++ source/backend/Backend.Infrastructure/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
