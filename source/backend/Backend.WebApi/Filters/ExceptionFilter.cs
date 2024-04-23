c6c4776c20f53509d2e164bba4ce6fe84345e28d
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-04-23 08:22:48 +0200

    added filter to handle exceptions

---
+++ source/backend/Backend.WebApi/Filters/ExceptionFilter.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.WebApi.Filters;
+
+using Backend.WebApi.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        var error = new AppError
+        {
+            StatusCode = 500,
+            Message = context.Exception.Message
+        };
+
+        context.Result = new ObjectResult(error) { StatusCode = 500 };
+    }
+}
