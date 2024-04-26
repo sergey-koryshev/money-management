5fc31127adbd9c50f1c9a0b7f35d5e8a9c3ea90e
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-04-26 08:47:30 +0200

    added initial migration

--- source/backend/Backend.Infrastructure/AppDbContext.cs
+++ source/backend/Backend.Infrastructure/AppDbContext.cs
@@ -1,7 +1,6 @@
 ﻿namespace Backend.Infrastructure;
 
 using Backend.Domain.Entities;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,64 +9,5 @@ public class AppDbContext : IdentityUserContext<User, int>
     public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
     {
         Database.EnsureCreated();
-    }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-
-        builder.Entity<User>().HasData(
-            new User
-            {
-                Id = 1,
-                FirstName = "User",
-                SecondName = "1",
-                Tenant = new Guid("22a11263-56da-4327-98b7-f99d6591ac3c"),
-                UserName = "user1@test.com",
-                NormalizedUserName = "user1@test.com".ToUpper(),
-                Email = "user1@test.com",
-                NormalizedEmail = "user1@test.com".ToUpper(),
-                PasswordHash = "AQAAAAIAAYagAAAAEGKSJpyLst59yBAmt6MXHpND7AhwsDUg4SvlCpkJWRTYcVnz0y1IowRarAAcqh56Dw==",
-                SecurityStamp = "CMUFAZEZFW52LZORGCWV3KDELEEJ2K5J",
-                ConcurrencyStamp = "c824a837-3d13-4ffa-9d6f-c9968047e698",
-                LockoutEnd = null,
-                LockoutEnabled = true,
-                AccessFailedCount = 0
-            },
-            new User
-            {
-                Id = 2,
-                FirstName = "User",
-                SecondName = "2",
-                Tenant = new Guid("f1d4515b-f201-4696-86b8-3580ad740ada"),
-                UserName = "user2@test.com",
-                NormalizedUserName = "user2@test.com".ToUpper(),
-                Email = "user2@test.com",
-                NormalizedEmail = "user2@test.com".ToUpper(),
-                PasswordHash = "AQAAAAIAAYagAAAAEAuVFkmqPoNhEDW7Bxsrt8M+16VWYYnMtl47iKZjQccyUyXEIg027L9GVYxN39ekWg==",
-                SecurityStamp = "EM3VWJZHCPH3MYEINTHQYHVCWEDDYCUV",
-                ConcurrencyStamp = "471a9303-4888-482b-abcf-6bce1e025719",
-                LockoutEnd = null,
-                LockoutEnabled = true,
-                AccessFailedCount = 0
-            },
-            new User
-            {
-                Id = 3,
-                FirstName = "User",
-                SecondName = "3",
-                Tenant = new Guid("f1d4515b-f201-4696-86b8-3080ad740ada"),
-                UserName = "user3@test.com",
-                NormalizedUserName = "user3@test.com".ToUpper(),
-                Email = "user3@test.com",
-                NormalizedEmail = "user3@test.com".ToUpper(),
-                PasswordHash = "AQAAAAIAAYagAAAAEGboD1FRWryugI0cZashzY9oX0HbYleTNgDuucjUHATdcsg596/Pvxgta8vgKy+ahw==",
-                SecurityStamp = "2OCDPDEX2FEYQFBHYR2NPSVUXRJP7NZU",
-                ConcurrencyStamp = "1850ad70-8df7-4c8d-9f6b-9c83b2bf6241",
-                LockoutEnd = null,
-                LockoutEnabled = true,
-                AccessFailedCount = 0
-            }
-        );
     }
 }
---
+++ source/backend/Backend.Infrastructure/Migrations/20240426064616_InitialMigration.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    SecondName = table.Column<string>(type: "text", nullable: true),
+                    Tenant = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    PasswordHash = table.Column<string>(type: "text", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "text", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    ClaimType = table.Column<string>(type: "text", nullable: true),
+                    ClaimValue = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "text", nullable: false),
+                    ProviderKey = table.Column<string>(type: "text", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    LoginProvider = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "FirstName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "SecondName", "SecurityStamp", "Tenant", "UserName" },
+                values: new object[,]
+                {
+                    { 1, 0, "c824a837-3d13-4ffa-9d6f-c9968047e698", "user1@test.com", "User", true, null, "USER1@TEST.COM", "USER1@TEST.COM", "AQAAAAIAAYagAAAAEGKSJpyLst59yBAmt6MXHpND7AhwsDUg4SvlCpkJWRTYcVnz0y1IowRarAAcqh56Dw==", "1", "CMUFAZEZFW52LZORGCWV3KDELEEJ2K5J", new Guid("22a11263-56da-4327-98b7-f99d6591ac3c"), "user1@test.com" },
+                    { 2, 0, "471a9303-4888-482b-abcf-6bce1e025719", "user2@test.com", "User", true, null, "USER2@TEST.COM", "USER2@TEST.COM", "AQAAAAIAAYagAAAAEAuVFkmqPoNhEDW7Bxsrt8M+16VWYYnMtl47iKZjQccyUyXEIg027L9GVYxN39ekWg==", "2", "EM3VWJZHCPH3MYEINTHQYHVCWEDDYCUV", new Guid("f1d4515b-f201-4696-86b8-3580ad740ada"), "user2@test.com" },
+                    { 3, 0, "1850ad70-8df7-4c8d-9f6b-9c83b2bf6241", "user3@test.com", "User", true, null, "USER3@TEST.COM", "USER3@TEST.COM", "AQAAAAIAAYagAAAAEGboD1FRWryugI0cZashzY9oX0HbYleTNgDuucjUHATdcsg596/Pvxgta8vgKy+ahw==", "3", "2OCDPDEX2FEYQFBHYR2NPSVUXRJP7NZU", new Guid("f1d4515b-f201-4696-86b8-3080ad740ada"), "user3@test.com" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+        }
+    }
+}
