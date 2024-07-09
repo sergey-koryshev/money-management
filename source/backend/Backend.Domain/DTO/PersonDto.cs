98ca84af5aa4d4e4fe871df717cfe1877604179f
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-07-09 17:57:26 +0200

    added Person's related classes

---
+++ source/backend/Backend.Domain/DTO/PersonDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Domain.DTO;
+
+public class PersonDto
+{
+    public int Id { get; set; }
+
+    public required string FirstName { get; set; }
+
+    public required string SecondName { get; set; }
+
+    public Guid Tenant { get; set; }
+}
---
+++ source/backend/Backend.Domain/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Backend.Domain.Entities;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Person
+{
+    public int Id { get; set; }
+
+    [Required]
+    public int UserId { get; set; }
+
+    [Required]
+    public required string FirstName { get; set; }
+
+    [Required]
+    public required string SecondName { get; set; }
+
+    [Required]
+    public Guid Tenant { get; set; }
+
+    public virtual ICollection<Category> CreatedCategories { get; set; } = new List<Category>();
+
+    public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
+}
