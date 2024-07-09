200dd2fc71fb602f4efa3d6eed239c0bef158bb7
Sergey Koryshev <sergey.koryshev@outlook.com>
2024-07-09 18:06:09 +0200

    implemented controller for categories

---
+++ source/backend/Backend.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿namespace Backend.WebApi;
+
+using Backend.Service;
+using Backend.WebApi.Results;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class CategoriesController
+{
+    private readonly ICategoriesService categoryService;
+
+    public CategoriesController(ICategoriesService categoriesService)
+    {
+        this.categoryService = categoriesService;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllCategories()
+    {
+        var categories = this.categoryService.GetAllCategories();
+        return new AppActionResult(categories);
+    }
+}
