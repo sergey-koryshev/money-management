f293b4dc741156f264922cdaab58bc79b72b7776
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-02-06 06:59:52 +0100

    Moved logic to check expense against filters to separate service

---
+++ source/frontend/src/app/modules/expenses/expenses.service.ts
@@ -0,0 +1,34 @@
+import { ExpensesStickyFilters, ExpensesStickyFilterType } from './pages/expenses-page/expenses-filters.model';
+import { Injectable } from '@angular/core';
+import { Expense } from '@app/models/expense.model';
+import { emptyFilter } from './pages/expenses-page/expenses-page.component';
+import { CreatedByFilterOptions } from '@app/models/enums/created-by-filter.enum';
+import { SharedFilterOptions } from '@app/models/enums/shared-filter.enum';
+import { UserService } from '@app/services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpensesService {
+
+  constructor(private userService: UserService) {}
+
+  testExpenseAgainstFilter(stickyFilters: ExpensesStickyFilters | undefined, expense: Expense) {
+    if (stickyFilters == null) {
+      return true;
+    }
+
+    if ((stickyFilters[ExpensesStickyFilterType.shared] == null ||
+      (stickyFilters[ExpensesStickyFilterType.shared].selectedValue?.value === emptyFilter.value ||
+        (stickyFilters[ExpensesStickyFilterType.shared].selectedValue?.value === SharedFilterOptions.Yes && expense.permittedPersons.length > 0) ||
+        (stickyFilters[ExpensesStickyFilterType.shared].selectedValue?.value === SharedFilterOptions.No && expense.permittedPersons.length === 0))) &&
+      ((stickyFilters[ExpensesStickyFilterType.createdBy] == null ||
+        (stickyFilters[ExpensesStickyFilterType.createdBy].selectedValue?.value === emptyFilter.value ||
+          stickyFilters[ExpensesStickyFilterType.createdBy].selectedValue?.value === CreatedByFilterOptions.Me && expense.createdBy.id === this.userService.user?.id ||
+            stickyFilters[ExpensesStickyFilterType.createdBy].selectedValue?.value === CreatedByFilterOptions.NotMe && expense.createdBy.id !== this.userService.user?.id)))) {
+      return true;
+    }
+
+    return false;
+  }
+}
