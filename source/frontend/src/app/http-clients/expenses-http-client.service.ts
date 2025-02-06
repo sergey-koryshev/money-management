4931d972d81ade14fe8df26ba059ea87a0238dde
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-02-06 08:24:19 +0100

    Corrected logic around filter in getExpenses function of ExpensesHttpClientService

--- source/frontend/src/app/http-clients/expenses-http-client.service.ts
+++ source/frontend/src/app/http-clients/expenses-http-client.service.ts
@@ -1,11 +1,10 @@
+import { ExpensesStickyFilterType, StoredExpensesStickyFilters } from './../modules/expenses/pages/expenses-page/expenses-filters.model';
 import { Expense } from '@app/models/expense.model';
 import { Injectable } from '@angular/core';
 import { BaseHttpClientService } from './base-http-client.service';
 import { ChangeExpenseParams, ExtendedExpenseName } from './expenses-http-client.model';
 import { Month } from '@app/models/month.model';
 import { of } from 'rxjs';
-import { defaultFilters } from '@app/modules/expenses/pages/expenses-page/expenses-page.component';
-import { ExpensesFilters } from '@app/modules/expenses/pages/expenses-page/expenses-filters.model';
 import { HttpParams } from '@angular/common/http';
 
 @Injectable({
@@ -15,20 +14,20 @@ export class ExpensesHttpClientService {
 
   constructor(private baseHttpClient: BaseHttpClientService) {}
 
-  getExpenses(selectedMonth: Month, filters?: ExpensesFilters) {
-    const additionalFilters = filters ?? defaultFilters
-
+  getExpenses(selectedMonth: Month, filters?: StoredExpensesStickyFilters) {
     let params = new HttpParams()
       .set('month', selectedMonth.month)
       .set('year', selectedMonth.year)
       .set('timeZone', Intl.DateTimeFormat().resolvedOptions().timeZone);
 
-    if (additionalFilters.createdBy.value != null) {
-      params = params.set('createdById', additionalFilters.createdBy.value as number);
-    }
+    if (filters != null) {
+      if (filters[ExpensesStickyFilterType.createdBy] != null && filters[ExpensesStickyFilterType.createdBy].value != null) {
+        params = params.set('createdById', filters[ExpensesStickyFilterType.createdBy].value!);
+      }
 
-    if (additionalFilters.shared.value != null) {
-      params = params.set('shared', !!additionalFilters.shared.value);
+      if (filters[ExpensesStickyFilterType.shared] != null && filters[ExpensesStickyFilterType.shared].value != null) {
+        params = params.set('shared', !!filters[ExpensesStickyFilterType.shared].value);
+      }
     }
 
     return this.baseHttpClient.get<Expense[]>('expenses', params);
