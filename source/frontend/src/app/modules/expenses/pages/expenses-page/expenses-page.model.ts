d733d2449bbc8cd5aeeb0f69532f2b9f3553a60b
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-02-20 19:45:13 +0100

    added model StoringExpensesStickyFilters for work with filters in more convenient way

---
+++ source/frontend/src/app/modules/expenses/pages/expenses-page/expenses-page.model.ts
@@ -0,0 +1,11 @@
+import { ExpensesStickyFilterType } from "@app/models/enums/expenses-sticky-filter-type.enum";
+import {
+  StickyFilterItem,
+  StoringStickyFilters
+} from "@components/sticky-filters/sticky-filters.model";
+
+export interface StoringExpensesStickyFilters extends StoringStickyFilters {
+  [ExpensesStickyFilterType.createdBy]?: StickyFilterItem<number | undefined>
+  [ExpensesStickyFilterType.shared]?: StickyFilterItem<number | undefined>
+  [ExpensesStickyFilterType.categories]?: StickyFilterItem<string | undefined>[]
+}
