642bd63ae3d31d0d13765aab90059fc338ea30a0
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-04-28 08:25:31 +0200

    Added migration to introduce announcements

---
+++ source/backend/Backend.Infrastructure/Migrations/20250426204336_Announcements.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Announcements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AnnouncementTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnnouncementTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Announcements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    HTML = table.Column<string>(type: "text", nullable: false),
+                    TimeStamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TypeId = table.Column<int>(type: "integer", nullable: false),
+                    Dismissible = table.Column<bool>(type: "boolean", nullable: true),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Announcements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Announcements_AnnouncementTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "AnnouncementTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DismissedAnnouncements",
+                columns: table => new
+                {
+                    AnnouncementId = table.Column<int>(type: "integer", nullable: false),
+                    PersonId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DismissedAnnouncements", x => new { x.AnnouncementId, x.PersonId });
+                    table.ForeignKey(
+                        name: "FK_DismissedAnnouncements_Announcements_AnnouncementId",
+                        column: x => x.AnnouncementId,
+                        principalTable: "Announcements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DismissedAnnouncements_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AnnouncementTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "PopUp" },
+                    { 2, "Alert" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Announcements_TypeId_Active",
+                table: "Announcements",
+                columns: new[] { "TypeId", "Active" },
+                unique: true,
+                filter: "\"Active\" = TRUE");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DismissedAnnouncements_PersonId",
+                table: "DismissedAnnouncements",
+                column: "PersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DismissedAnnouncements");
+
+            migrationBuilder.DropTable(
+                name: "Announcements");
+
+            migrationBuilder.DropTable(
+                name: "AnnouncementTypes");
+        }
+    }
+}
