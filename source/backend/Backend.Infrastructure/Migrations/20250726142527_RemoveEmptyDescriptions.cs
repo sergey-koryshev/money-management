f7189a7721a9ce5d5a169f583953c69b6c7975d4
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-07-26 16:39:16 +0200

    Added migration to remove empty expense descriptions

---
+++ source/backend/Backend.Infrastructure/Migrations/20250726142527_RemoveEmptyDescriptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEmptyDescriptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE ""Expenses""
+                SET ""Description"" = NULL
+                WHERE ""Description"" IS NOT NULL AND TRIM(""Description"") = ''");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
