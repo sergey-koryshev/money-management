e8c8cbd3b165d181d8a77a5732e5d434be4ed0f0
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-11-28 08:46:58 +0100

    Added support for floating menu in table

--- source/frontend/src/app/components/table/table.module.ts
+++ source/frontend/src/app/components/table/table.module.ts
@@ -2,11 +2,18 @@ import { TableComponent } from './table.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SortableHeaderDirective } from './sortable-header.directive';
+import { NgbDropdownModule, NgbAccordionModule } from "@ng-bootstrap/ng-bootstrap";
+import { ContextMenuModule } from '@perfectmemory/ngx-contextmenu';
+import { RoundButtonModule } from '../round-button/round-button.module';
 
 @NgModule({
   declarations: [SortableHeaderDirective, TableComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbDropdownModule,
+    ContextMenuModule,
+    NgbAccordionModule,
+    RoundButtonModule
   ],
   exports: [TableComponent]
 })
