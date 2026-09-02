444a9e70b9203008ab50c42b1b154d9efda2ecf9
Sergey Koryshev <sergey.koryshev@outlook.com>
2026-09-02 08:38:34 +0200

    Remove title from announcement PopUp (v0.4) (#118)

--- source/frontend/src/app/layout/notice-dialog/notice-dialog.component.ts
+++ source/frontend/src/app/layout/notice-dialog/notice-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Announcement } from '@app/models/announcement.model';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
